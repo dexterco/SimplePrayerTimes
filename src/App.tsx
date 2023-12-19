2257571eb37d5d35ea83376b29477438adf7a94e
Dexterco <192400010@gift.edu.pk>
2023-12-19 12:29:26 +0500

    Receive Share intent Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import MyComponent from './components/MyComponent';
-import {
- Linking
-} from 'react-native';
+import ReceiveSharingIntent from 'react-native-receive-sharing-intent';
+
+
 function App(): React.JSX.Element {
-  useEffect(() => {
-    const handleShareIntent = async () => {
-      try {
-        // Check if the app was opened by a sharing intent
-        const initialUrl = await Linking.getInitialURL();
-        console.log(initialUrl);
-        
   
-      } catch (error) {
-        console.error('Error handling share intent:', error);
-      }
-    };
-
-    handleShareIntent();
-
-
-  }, []);
-
   return (
     <Provider store={store}>
     <MyComponent />
--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,11 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { setUser } from '../slices/userSlice';
+import { useEffect, useState } from 'react';
 import {
     Text,
     View,
-    Button
+    Button,
+    Image
   } from 'react-native';
 import ShowName from './showName';
+import ReceiveSharingIntent from 'react-native-receive-sharing-intent';
 const MyComponent = () => {
   const user = useSelector((state:any) => state.user.usern);
   const dispatch = useDispatch();
@@ -15,13 +18,35 @@ const MyComponent = () => {
     dispatch(setUser({ name: 'John' }));
   };
 
+  const [imageUrl,setImageUri] = useState("")
+useEffect(()=>{
+  ReceiveSharingIntent.getReceivedFiles((files:any) => {
+    // files returns as JSON Array example
+    //[{ filePath: null, text: null, weblink: null, mimeType: null, contentUri: null, fileName: null, extension: null }]
+     console.log(files[0].contentUri);
+     setImageUri(files[0].contentUri)
+     
+  }, 
+  (error:any) =>{
+    console.log(error);
+  }, 
+  'ShareMedia' // share url protocol (must be unique to your app, suggest using your apple bundle id)
+  );
+  return()=>{
+    ReceiveSharingIntent.clearReceivedFiles();
+  }
+},[])
+
   return (
     <View>
       <Text>{user ? `Welcome, ${user.name}` : 'Please log in'}</Text>
       <Button title="Login" onPress={handleLogin} />
       <ShowName/>
+      <Image source={{ uri: imageUrl }} style={{ width: 400,height:400,}} />
     </View>
   );
 };
+
+
 
 export default MyComponent;
